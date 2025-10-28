0120cad5e41265b3b5b96bf69e7e133adde4b488
Ed_Newmann123 <eduardoneumannpb@gmail.com>
2025-10-27 22:02:30 -0300

    Refactor code structure for improved readability and maintainability

--- src/routes/RootNavigation.tsx
+++ src/routes/RootNavigation.tsx
@@ -1,7 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "../components/Layout";
 import Home from "../pages/Home";
-import Games from "../pages/Games";\nimport Login from "../pages/Login";\nimport Register from "../pages/Register";
+import Games from "../pages/Games";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
 
 export default function RootNavigation() {
     return (
@@ -9,11 +11,12 @@ export default function RootNavigation() {
             <Routes>
                 <Route path="/" element={<Layout />} >
                     <Route index element={<Home />} />
-                    <Route path="games" element={<Games />} />\n                    <Route path="login" element={<Login />} />\n                    <Route path="register" element={<Register />} />
+                    <Route path="games" element={<Games />} />
+                    <Route path="login" element={<Login />} />
+                    <Route path="register" element={<Register />} />
                     {/* Adicionar rota para a roleta aqui, se for diferente de /games */}
                 </Route>
             </Routes>
         </BrowserRouter>
     );
 }
-
